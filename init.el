80b1cb70de0e5776b1330129d583bb2c0ebbb197
dzzdzzdz <danielzhaozd@gmail.com>
2020-05-16 22:25:06 +0200

    done 3

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key try use-package))))
+ '(package-selected-packages (quote (org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -31,3 +31,8 @@
   :ensure t
   :config (which-key-mode))
 
+;; Org-mode stuff
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
