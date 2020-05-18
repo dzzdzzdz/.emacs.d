831c218b77f466c0d211735d9d33438de39c957b
dzzdzzdz <danielzhaozd@gmail.com>
2020-05-18 12:38:55 +0200

    done20

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (undo-tree htmlize which-key try use-package tabbar orgalist org-bullets counsel color-theme-modern color-theme auto-complete ace-window))))
+    (iedit expand-region aggressive-indent hungry-delete beacon undo-tree htmlize which-key try use-package tabbar orgalist org-bullets counsel color-theme-modern color-theme auto-complete ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
